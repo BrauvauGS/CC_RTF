bc9babb32e9894fd019e749b60ea77d8e9d51d89
kev <brauvaugamestudios@gmail.com>
2025-04-13 19:15:43 +0200

    dev bootloader

--- src/bootloader.lua
+++ src/bootloader.lua
@@ -1,7 +1,11 @@
 local logger = require("src.Modules.System.logger")
+local version = "1.0.0"
 
 local osUrl = "https://raw.githubusercontent.com/BrauvauGS/CC_RTF/refs/heads/dev/src/RTF_OS/RTF_os.lua"
 local osPath = "/RTF/src/RTF_OS/RTF_os.lua"
+
+--obj
+local ConsolLog = logger:new()
 
 
 -- Main boot function
@@ -9,8 +13,8 @@ function boot()
     term.clear()
     term.setCursorPos(1, 1)
     term.setTextColor(colors.cyan)
-    print("** RTF Bootloader **")
-
+    print("** RTF Bootloader " .. "[" ..version.."]".." **")
+    ConsolLog:log("S","Logger init ok")
 end
 
 -- Start the bootloader
